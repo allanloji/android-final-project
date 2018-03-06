8f81d3b80a491b50085459da0cb5b1a95b9612fe
Allan Lopez <allanloji@gmail.com>
2018-03-05 22:47:19 -0600

    Seleccion de idiomas creado

---
+++ app/src/main/java/com/allanloji/language_cast/LanguageSelectionActivity.java
@@ -0,0 +1,13 @@
+package com.allanloji.language_cast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LanguageSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_language_selection);
+    }
+}
