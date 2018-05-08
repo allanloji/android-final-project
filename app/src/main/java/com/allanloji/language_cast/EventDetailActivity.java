e16a0d072f94a88a7af2067f72cde11d840378ca
Allan López <allanloji@gmail.com>
2018-05-07 23:34:53 -0500

    Add youtube video

--- app/src/main/java/com/allanloji/language_cast/EventDetailActivity.java
+++ app/src/main/java/com/allanloji/language_cast/EventDetailActivity.java
@@ -13,12 +13,20 @@ import android.widget.Toast;
 
 import com.allanloji.language_cast.pojo.News;
 import com.facebook.drawee.view.SimpleDraweeView;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 
-public class EventDetailActivity extends Activity {
+public class EventDetailActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
     private TextView title, direction, date, description;
     private Button bttn;
     private SimpleDraweeView image;
+    private YouTubePlayerView youTubePlayerView;
+    private String key="AIzaSyCd8f1McFmDKTTH5pIWYu7MbONzct0Wh2I";
+    private String uri = "k0FFKbI6VLk";
+    private YouTubePlayer player;
 
     private String eventTitle, eventDirection, eventDate, eventDescription, eventImage, eventId;
 
@@ -63,7 +71,22 @@ public class EventDetailActivity extends Activity {
             }
         }
 
+        youTubePlayerView = (YouTubePlayerView) findViewById(R.id.videoView);
+        youTubePlayerView.initialize(key, this);
+    }
 
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        this.player = youTubePlayer;
+        if(!b){
+            youTubePlayer.cueVideo(uri);
+        }
+
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        Toast.makeText(this, "Youtube Error Not Available", Toast.LENGTH_LONG).show();
 
     }
 
